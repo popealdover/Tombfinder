31030e62f99c97bc079d4a55d841f9c162e0dca8
popealdover <114934078+popealdover@users.noreply.github.com>
2023-05-31 15:55:54 +0800

    Update login.php

--- login.php
+++ login.php
@@ -31,7 +31,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
             $_SESSION['user_name'] = $row['user_name'];
             $_SESSION['name'] = $row['name'];
             $_SESSION['id'] = $row['id'];
-            header("Location: home.php");
+            header("Location: index.php");
             exit();
         }else{
             header("Location: index2.php?error=Incorrect User name or Password!");
